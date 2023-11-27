39647d45e10861c1f1ec53e4d0e76c20a36aae89
KINGBOTs1 <ds767365@gmail.com>
2023-11-27 11:34:04 +0800

    fix accestoken

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -4,7 +4,7 @@
 docker build -t ghcr.io/kingxdeni/karsajobs-ui:latest .
 
 # Perintah untuk login ke GitHub Container Registry menggunakan Personal Access Token
-echo "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" | docker login ghcr.io -u kingxdeni --password-stdin
+echo "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" | docker login ghcr.io -u kingxdeni --password-stdin
 
 # Perintah untuk push image ke GitHub Packages
 docker push ghcr.io/kingxdeni/karsajobs-ui:latest
