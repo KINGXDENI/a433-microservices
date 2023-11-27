d948eafb0246b5ca20b0a9ce9b47601703c4930f
KINGBOTs1 <ds767365@gmail.com>
2023-11-27 11:58:29 +0800

    Update build_push_image_karsajobs.sh

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Token disimpan dalam file txt
+TOKEN=$(cat ../token.txt)
+
 # Perintah untuk build Docker image dari Dockerfile
 docker build -t ghcr.io/kingxdeni/karsajobs:latest .
 
-# Perintah untuk login ke GitHub Container Registry menggunakan Personal Access Token
-echo "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" | docker login ghcr.io -u kingxdeni --password-stdin
+# Perintah untuk login ke GitHub Container Registry menggunakan Personal Access Token dari file
+echo "$TOKEN" | docker login ghcr.io -u kingxdeni --password-stdin
 
 # Perintah untuk push image ke GitHub Packages
 docker push ghcr.io/kingxdeni/karsajobs:latest
