bc2abf069fddd0b0ef4988a5fc3188956fc37499
KINGBOTs1 <ds767365@gmail.com>
2023-11-27 11:27:38 +0800

    karsajobs

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Perintah untuk build Docker image dari Dockerfile
+docker build -t ghcr.io/kingxdeni/karsajobs:latest .
+
+# Perintah untuk login ke GitHub Container Registry menggunakan Personal Access Token
+echo "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" | docker login ghcr.io -u kingxdeni --password-stdin
+
+# Perintah untuk push image ke GitHub Packages
+docker push ghcr.io/kingxdeni/karsajobs:latest
