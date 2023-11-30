1708396743e88c697b83cb7916ea285c2408b5f7
KINGBOTs1 <ds767365@gmail.com>
2023-12-01 02:09:06 +0800

    add

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
+// Menggunakan dotenv untuk mengatur konfigurasi lingkungan dari file .env
 require('dotenv').config()
 
+// Mengimpor express dan membuat instance aplikasi
 const express = require("express");
 const app = express();
 
+// Menggunakan body-parser untuk menangani payload JSON dalam permintaan
 const bp = require("body-parser");
 app.use(bp.json());
 
+// Mengimpor amqplib untuk berinteraksi dengan RabbitMQ
 const amqp = require("amqplib");
-const amqpServer = process.env.AMQP_URL;
+const amqpServer = process.env.AMQP_URL; // Mengambil URL RabbitMQ dari variabel lingkungan
 var channel, connection;
 
+// Fungsi untuk terhubung ke RabbitMQ dan membuat saluran (channel)
 connectToQueue();
 
 async function connectToQueue() {
@@ -24,25 +29,31 @@ async function connectToQueue() {
     }
 }
 
+// Menangani permintaan POST ke endpoint /order
 app.post("/order", (req, res) => {
-    const { order } = req.body;
+    const {
+        order
+    } = req.body;
     createOrder(order);
     res.send(order);
 });
 
+// Fungsi untuk membuat pesanan dan mengirimkannya ke antrian RabbitMQ
 const createOrder = async order => {
     const queue = "order";
     await channel.sendToQueue(queue, Buffer.from(JSON.stringify(order)));
-    console.log("Order succesfully created!")
-    process.once('SIGINT', async () => { 
+    console.log("Order successfully created!")
+
+    // Menangani sinyal SIGINT (Ctrl+C) untuk menutup saluran dan koneksi saat keluar
+    process.once('SIGINT', async () => {
         console.log('got sigint, closing connection');
         await channel.close();
-        await connection.close(); 
+        await connection.close();
         process.exit(0);
     });
 };
 
+// Menjalankan server pada port yang ditentukan dalam variabel lingkungan
 app.listen(process.env.PORT, () => {
     console.log(`Server running at ${process.env.PORT}`);
 });
-
